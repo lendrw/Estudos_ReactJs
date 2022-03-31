c43fb2d0d1338fca6e2177fdce96c79cefef1b0c
lendrw <leandro.cabral@alunos.ufersa.edu.br>
2022-03-30 23:19:10 -0300

    criando componentes

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import Frase from './components/Frase';
 
 function App() {
   return (
     <div className="App">
       <h1>Olá</h1>
+      <HelloWorld/>
+      <Frase/>
     </div>
   );
 }
