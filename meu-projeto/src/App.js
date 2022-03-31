c732708db065ffb868cc4885cca51558025be562
lendrw <leandro.cabral@alunos.ufersa.edu.br>
2022-03-31 01:07:44 -0300

    ajustando css

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,14 +1,21 @@
 
 import './App.css';
-import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
 
 function App() {
   return (
     <div className="App">
-      <h1>Olá</h1>
-      <HelloWorld/>
+      <h1>Testando CSS</h1>
       <Frase/>
+      <Frase/>
+      <SayMyName nome="Leandro"/>
+      <Pessoa 
+      nome="Carlos"
+      idade="20"
+      profissao="pedreiro"
+      foto="https://via.placeholder.com/150"/>
     </div>
   );
 }
